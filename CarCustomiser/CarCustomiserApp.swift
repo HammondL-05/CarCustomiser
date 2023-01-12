7f8b9d727618493322a9102d3397acda20e6733d
Leo Hammond <hammond.l@etoncollege.org.uk>
2023-01-12 20:56:11 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Leo Hammond on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
